0fc5cca960370873c302c2ca131f73df659cf9b1
sidewalksndskeletons <sidewalksndskeletons@gmail.com>
2026-05-10 14:50:23 +0000

    Create mela-aliases.sh

---
+++ mela-aliases.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -v'
+alias mkdir='mkdir -p'
+alias psaf='ps auxf'
+alias ping='ping -c 10'
+alias less='less -R'
+alias cls='clear'
+alias cd..='cd ..'
+alias rmdir='rmdir -v'
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
